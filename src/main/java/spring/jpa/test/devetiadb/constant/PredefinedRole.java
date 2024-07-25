e42a4ed1b2202afa1c833a40941d5ad79cf72a60
Tran Hoang Quan <quan02042004@gmail.com>
2024-07-25 23:17:34 +0700

    feat: add unique field

---
+++ src/main/java/spring/jpa/test/devetiadb/constant/PredefinedRole.java
@@ -0,0 +1,9 @@
+package spring.jpa.test.devetiadb.constant;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class PredefinedRole {
+    public static final String USER_ROLE = "USER";
+    public static final String ADMIN_ROLE = "ADMIN";
+}
